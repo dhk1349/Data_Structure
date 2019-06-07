91d80f91c598806601dcb8d0f62e584713bf62ee
dhk1349 <dhk1349@naver.com>
2019-06-08 00:38:02 +0900

    Last version

---
+++ Final/Application.cpp
@@ -0,0 +1,371 @@
+#include "Application.h"
+//#include"Unsorted_Iter.h"
+template<typename T>
+class Unsorted_Iterator;
+
+// Program driver.
+void Application::Run2()
+{
+	while (1)
+	{
+		cout << "\n\t=====================\n";
+		m_curFolder->DisplayPathOnScreen();
+		cout << "\t=====================";
+		m_Command = GetCommand2();
+		switch (m_Command)
+		{
+		case 1:		
+			DisplayAll(); 
+			break;
+		case 2:		
+			FolderOption();
+			break;
+		case 3:
+			FileOption();
+			break;
+		case 4:		
+			SearchOption();
+			break;
+		case 5:
+			RecentRecord();
+			break;
+		case 6:	
+			MoveBack();
+			break;
+		case 7:		
+			MoveForward();
+			break;
+		case 0:
+			return;
+		default:
+			cout << "\tIllegal selection...\n";
+			break;
+		}
+	}
+}
+
+void Application::FolderOption() {
+	int comm;
+	cout << "\t========Folder Command========\n";
+	cout << "\t   1. 폴더 생성\n";
+	cout << "\t   2. 폴더 삭제\n";
+	cout << "\t   3. 폴더 열기\n";
+	cout << "\t   4. 폴더 속성\n";
+	cout << "\t   5. 폴더 복사\n";
+	cout << "\t   6. 폴더 잘라내기\n";
+	cout << "\t   7. 폴더 붙여넣기\n";
+	cout << "\t   8. 폴더 이름 바꾸기\n";
+	cout << "\t   9. 나가기\n";
+	cout << endl << "\t Choose a Command--> ";
+	cin >> comm;
+	switch (comm) {
+		case 1	:
+			NewFolder();
+			break;
+		case 2:
+			DeleteFolder();
+			break;
+		case 3:
+			OpenFolder();
+			break;
+		case 4:
+			DisplayProperty();
+			break;
+		case 5:
+			CopyFolder();
+			break;
+		case 6:
+			CutFolder();
+			break;
+		case 7:
+			PasteFolder();
+			break;
+		case 8:
+			ChangeFolderName();
+			break;
+		case 9:
+			break;
+		default:
+			cout << "\tIllegal Selection \n";
+	}
+}
+
+void Application::FileOption() {
+	int comm;
+	cout << "\t========File Command========\n";
+	cout << "\t   1. 텍스트 파일 생성\n";
+	cout << "\t   2. 텍스트 파일 열기\n";
+	cout << "\t   3. 파일 이름 바꾸기\n";
+	cout << "\t   4. 파일 삭제\n";
+	cout << "\t   5. 음악 저장하기\n";
+	cout << "\t   6. 음악 재생하기\n";
+	cout << "\t   7. 나가기\n";
+	cout << endl << "\t Choose a Command--> ";
+	cin >> comm;
+	switch (comm) {
+	case 1:
+		CreateText();
+		break;
+	case 2:
+		OpenText();
+		break;
+	case 3:
+		ChangeFilename();
+		break;
+	case 4:
+		Delete_File();
+		break;
+	case 5:
+		ImportMusic();
+		break;
+	case 6:
+		PlayMusic();
+		break;
+	case 7:
+		break;
+	default:
+		cout << "\tIllegal Selection \n";
+	}
+}
+
+void Application::SearchOption() {
+	int comm;
+	cout << "\t========Search Command========\n";
+	cout << "\t   1. 전역 검색\n";
+	cout << "\t   2. 나가기\n";
+	cout << endl << "\t Choose a Command--> ";
+	cin >> comm;
+	switch (comm) {
+	case 1:
+		GlobalSearch();
+		break;
+	case 2:
+		break;
+	default:
+		cout << "\tIllegal Selection \n";
+	}
+}
+
+int Application::DisplayAll() {
+	m_curFolder->DisplayAll();
+	//m_curFolder->DisplayAllFileName();
+	return 1;
+}
+
+int Application::GetCommand2() {
+	//DisplayAllFolders();
+	int command;
+	cout << endl << endl;
+	cout << "\t-------- Command -------- " << endl;
+	cout << "\t   1 : 하부 목록 보기" << endl;
+	cout << "\t   2 : 폴더 옵션" << endl;
+	cout << "\t   3 : 파일 옵션" << endl;
+	cout << "\t   4 : 검색 옵션" << endl;
+	cout << "\t   5 : 최근 기록" << endl;
+	cout << "\t   6 : 상위 폴더로 이동(앞으로 이동)" << endl;
+	cout << "\t   7 : 하위 폴더로 이동" << endl;
+	cout << "\t   0 : 종료" << endl;
+
+	cout << endl << "\t Choose a Command--> ";
+	cin >> command;
+	cout << endl;
+
+	return command;
+}
+
+int Application::NewFolder() 
+{
+	int result = m_curFolder->AddFolder();
+	return result;
+
+}
+int Application:: RetrieveFolderByName() 
+{
+	m_curFolder->RetrieveFoldersByName();
+	return 1;
+}
+//Delete있음
+int Application::DisplayProperty() 
+{
+	m_curFolder->DisplayRecordOnScreen();
+	return 1;
+}
+
+int Application::DeleteFolder() {
+	int result=m_curFolder->DeleteFolders();
+	return result;
+}
+
+int Application::Delete_File() {
+	int result = m_curFolder->DeleteFiles();
+	return result;
+}
+
+//하부폴더로 들어가는 함수.
+
+int Application::OpenFolder() {
+	DisplayAll();
+	FolderType* temp;
+	temp = new FolderType;
+	//int index = m_curFolder->SearchFolder(temp);
+	m_curFolder = m_curFolder->SearchAllType(temp);
+	if(m_curFolder!= NULL) {
+		//m_curFolder->Setname(temp->GetName());  이렇게 하면 원래의 상위폴더의 이름이 바뀌어버림..
+		//m_curFolder=m_curFolder->SearchGivenFolder(temp);
+		//m_curFolder = temp; //이부분이 잘못되었다.
+		
+		Addque(m_curFolder);
+		UpperLower.EqualizeIndex();
+		return 1;
+	}
+	
+	else {
+		m_curFolder = UpperLower.Get(UpperLower.Getindex());
+		cout << "해당 하부 폴더가 없습니다.\n";
+		return 0;
+	}
+}
+
+int Application::Addque(AllType* indata) {
+	m_RecentlyFolder.enque(indata);
+	UpperLower.push(indata);
+	return 1;
+}
+
+int Application::Subque() {
+	FolderType temp;
+	m_RecentlyFolder.deque();
+	return 1;
+}
+int Application::RecentRecord() {
+	cout << "\t=====최근 열어본 폴더=====\n";
+	m_RecentlyFolder.print();
+	cout << "\t==========================\n";
+	return 1;
+}
+
+int Application::MoveBack() {
+	if (UpperLower.Getindex()>0) {
+		int index = UpperLower.Getindex() - 1;
+		m_curFolder = UpperLower.Get(index);
+		m_RecentlyFolder.enque(m_curFolder);
+		UpperLower.DecreaseIndex();
+	}
+	else {
+		cout << "\t더 이상 이동할 수 없습니다.\n";
+	}
+	return 1;
+}
+
+int Application::MoveForward() {
+	if (UpperLower.Getindex2() > UpperLower.Getindex()) {
+		UpperLower.IncreaseIndex();
+		m_curFolder = UpperLower.Get(UpperLower.Getindex());
+	}
+	else {
+		cout << "\t뒤로 갈 수 없습니다!!\n";
+	}
+	return 1;
+}
+
+int Application::CreateText() {
+	m_curFolder->Addtextfile();
+	return 1;
+}
+
+int Application::OpenText() {
+	m_curFolder->Opentext();
+	return 1;
+}
+
+int Application::ChangeFolderName() {
+	m_curFolder->ChangeSubfolderName();
+	m_curFolder->DisplayAll();
+	return 1;
+}
+int Application::ChangeFilename() {
+	m_curFolder->ChangeFileName();
+	m_curFolder->DisplayAll();
+	return 1;
+}
+
+
+void Application::CopyFolder() {
+	DisplayAll();
+	AllType* temp;
+	temp = new FolderType;
+	AllType* copy(m_curFolder->SearchAllType(temp));
+	copyspace = copy;
+	//지정한 폴더를 temp에다가 복사해두었다.
+}
+
+void Application::CutFolder() {
+	DisplayAll();
+	AllType* temp;
+	temp = new FolderType;
+	copyspace =m_curFolder->SearchAllType(temp);
+
+	m_curFolder->DeleteFolders2(temp);
+}
+
+void Application::PasteFolder() {
+	if (copyspace->GetName()!="") {
+		m_curFolder->PasteFolder(copyspace);
+	}
+	else {
+		cout << "\t복사된 내용이 없습니다!!\n";
+	}
+}
+
+void Application::ImportMusic() {
+	m_curFolder->IncludeMusic();
+}
+
+int Application::PlayMusic() {
+	m_curFolder->DisplayAll();
+	cout << "\t재생할 음악을 선택하세요(음악이 아니면 재생되지 않음)\n ";
+	AllType* temp;
+	temp = new FileType;
+	temp = m_curFolder->SearchAllType(temp);
+	if (temp!=NULL) {
+		if (temp->GetExt()=="wav") {
+			//음악재생
+			cout << "\t아무 키나 누르면 재생이 종료됩니다.\n";
+
+			
+			string inname = temp->GetName() + ".wav";
+
+			char buffer[MAX_PATH];
+			GetModuleFileNameA(NULL, buffer, MAX_PATH);
+			string::size_type pos = string(buffer).find_last_of("\\/");
+			inname = string(buffer).substr(0, pos) + "\\" + inname;
+			LPCSTR song = inname.c_str();
+
+			PlaySound(song, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+			while (!_kbhit());
+			PlaySound(NULL, 0, 0);
+			return 1;
+		}
+		else {
+			cout << "해당 파일의 형식은: "<<temp->GetExt()<<"\n";
+			cout << "\t형식이 일치하지 않습니다.\n";
+			return 1;
+		}
+	}
+	else { cout << "\t해당 파일을 찾을 수 없습니다.\n"; return 1; }
+}
+
+int Application::GlobalSearch() {
+	cout << "\tGlobal Search\n";
+	AllType *item=new FolderType;
+	item->SetNameFromKB();
+	SearchBox->push(&m_RootFolder);//시작할 때  루트 폴더의 주소 값을 넣는다.
+	FolderType *temp;
+	do {
+		temp = SearchBox->pop();
+		temp->SearchAllType(item);
+		temp->PushAllFolder(SearchBox);
+	} 
+	while (SearchBox->Getindex()!=-1);
+	SearchBox->InitializeIndex();
+}
---
+++ Final/Stack.h
@@ -0,0 +1,188 @@
+#pragma once
+
+#ifndef _STACK_H
+#define _STACK_H
+#include<iostream>
+using namespace std;
+
+template <typename T>
+class Stack {
+private:
+	T *list[100];
+	int index1;
+	int index2;
+public:
+
+	/*
+	* default constructor
+	*/
+	Stack() { index1 = -1; index2 = -1; }
+
+	/*
+	* destructor
+	*/
+	~Stack() {}
+
+	/**
+*	@brief 스택에 정보를 추가한다.
+*	@pre	temp는 포인터 형식이다.
+*	@post	스택에 temp가 입력된다.
+*	@param 스택에 입력할 정보.
+*	@return	성공하면 1을 반환한다. 스택 오버플로우가 생기면 0 반환.
+*/
+	int push(T* temp);
+
+	T* pop();
+
+	/**
+*	@brief 스택의 크기를 정보를 제공 (길이-1)
+*	@pre	없음
+*	@post	없음
+*	@return 스택의 크기를 리턴(index)
+*/
+	int Getindex();
+
+	/**
+*	@brief index2 정보를 제공
+*	@pre	없음
+*	@post	없음
+*	@return index2
+*/
+	int Getindex2();
+
+	/**
+*	@brief Index2값에 index1값을 넣는다.
+*	@pre	없음
+*	@post	index2=index1;
+*	@return 1
+*/
+	int EqualizeIndex();
+
+	/**
+*	@brief in번째 인덱스의 정보를 제공.
+*	@pre	없음
+*	@post	in번째 인덱스의 정보가 반환된다.
+*	@return	in번 째 데이터를 포인터 형식으로 반환한다.
+*/
+	T* Get(int in);
+
+	/**
+*	@brief 스택의 크기(index)를 하나 증가한다.
+*	@pre	없음
+*	@post	index+=1;
+*	@return	1을 반환한다.
+*/
+	int IncreaseIndex();
+
+	/**
+*	@brief 스택의 크기(index)를 하나 감소한다.
+*	@pre	없음
+*	@post	index-=1;
+*	@return	1을 반환한다.
+*/
+	int DecreaseIndex();
+
+	/**
+*	@brief index2의 크기를 설정한다..
+*	@pre	없음
+*	@post	index=innumber;
+*	@params innumber는 index2의 숫자가 된다.
+*	@return	1을 반환한다.
+*/
+	int SetIndex2(int innumber);
+
+	/**
+*	@brief 스택에 있는 데이터를 순차적으로 화면에 출력. 충복된 데이터
+*	@pre	없음
+*	@post	스택의 데이터가 모두 출력
+*	@return	없음.
+*/
+	void print();
+
+	void InitializeIndex() {
+		index1 = -1;
+		index2 = -1;
+	}
+};
+
+template<typename T>
+int Stack<T>::push(T* temp) {
+	if (index1 < 10) {//더 넣을 수 있음.
+		/*
+		for (int i = 0; i < index1 + 1; i++) {//겹치는 거 있나 확인.
+			if (temp->GetName() == list[i]->GetName()) {//겹칠 때
+				int j;
+				for (j = i; j < index1; j++) {//삭제
+					list[j] = list[j + 1];
+				}
+				list[index1] = temp;
+				return 1;
+			}
+		}
+		*/
+		index1 += 1;
+		list[index1] = temp;
+		return 1;
+	}
+	else//더 넣을 수 없음
+	{
+		return 0;
+	}
+}
+
+template<typename T>
+T* Stack<T>::pop() {
+	DecreaseIndex();
+	return Get(index1+1);
+}
+
+
+template<typename T>
+int Stack<T>::Getindex() {
+	return index1;
+}
+
+template<typename T>
+int Stack<T>::Getindex2() {
+	return index2;
+}
+
+template<typename T>
+int Stack<T>::IncreaseIndex() {
+	index1 += 1;
+	return 1;
+}
+
+template<typename T>
+int Stack<T>::DecreaseIndex() {
+	index1 -= 1;
+	return 1;
+}
+
+template<typename T>
+int Stack<T>::SetIndex2(int innumber) {
+	index2 = innumber;
+	return 1;
+}
+
+template<typename T>
+int Stack<T>::EqualizeIndex() {
+	index2 = index1;
+	return 1;
+}
+template<typename T>
+T* Stack<T>::Get(int in) {
+	return list[in];
+}
+
+
+template<typename T>
+void Stack<T>::print() {
+	for (int i = 0; i < index1 + 1; i++) {
+		cout << "\t" << i + 1 << ":  ";
+		cout << list[i]->GetName() << endl;;
+	}
+}
+
+
+#endif _
